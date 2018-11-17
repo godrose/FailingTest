b7485ce4f1ffbcdcea73f951fecf962428419d7d
Gennady Verdel <godrose@gmail.com>
2018-11-17 15:20:44 +0200

    Added all tests.

--- ThisFeatureShouldBeGreen.cs
+++ ThisFeatureShouldBeGreen.cs
@@ -30,5 +30,52 @@ namespace FailingTest
             };
             source.Clear();
         }
+
+        [Fact]
+        public void
+            WhenCollectionIsCreatedWithRangeAndSingleItemAndSourceIsUpdatedWithRemoveRange_ThenSingleNotificationIsRaisedWithAllWrappers
+            ()
+        {
+            var source = new RangeObservableCollection<object>();
+            var items = new[] { new object() };
+            var numberOfTimes = 0;
+
+            var collection = new WrappingCollection(isBulk: true)
+            {
+                FactoryMethod = o => o
+            };
+            collection.AddSource(source);
+            source.AddRange(items);
+            collection.CollectionChanged += (sender, args) =>
+            {
+                args.OldItems.Should().BeEquivalentTo(items);
+                numberOfTimes++;
+                numberOfTimes.Should().Be(1);
+            };
+            source.RemoveRange(items);
+        }
+
+        [Fact]
+        public void
+            WhenCollectionIsCreatedWithRangeAndSourceIsUpdatedWithAddRange_ThenSingleNotificationIsRaisedWithAllWrappers
+            ()
+        {
+            var source = new RangeObservableCollection<object>();
+            var items = new[] { new object(), new object() };
+            var numberOfTimes = 0;
+
+            var collection = new WrappingCollection(isBulk: true)
+            {
+                FactoryMethod = o => o
+            };
+            collection.AddSource(source);
+            collection.CollectionChanged += (sender, args) =>
+            {
+                args.NewItems.Should().BeEquivalentTo(items);
+                numberOfTimes++;
+                numberOfTimes.Should().Be(1);
+            };
+            source.AddRange(items);
+        }
     }
 }
